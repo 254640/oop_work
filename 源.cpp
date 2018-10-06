9a2303e459f9e22db81e47f2e28c116c686d4e39
马小玉 <2546409766@qq.com>
2018-10-06 14:43:53 +0800

    循环

--- 源.cpp
+++ 源.cpp
@@ -1,148 +1,185 @@
-//输出大写的十六进制
-
-#include<iostream>
-#include<windows.h>
-using namespace std;
-#include<iomanip>//该头文件中定义了一些控制流输出格式的函数，默认情况下为十进制
-void main()
-{
-	int i = 0x2F;
-	int j = 255;
-	cout << i << endl;
-	cout << hex << i << endl;
-	cout << hex << j << endl;
-	cout << hex << setiosflags(ios::uppercase) << j << endl;//hex可将其设置为十六进制的输出
-	system("pause");
-}
-
-
-//求逻辑表达式的值
+//while(表达式)语句：实现累加  注：表达式不得为空，循环体中必须有语句避免陷入死循环；
 #include<iostream>
 #include<windows.h>
 using namespace std;
 void main()
 {
-	int i = 5, j = 8, k = 12, l = 4, x1, x2;
-	x1 = i > j&&k > l;
-	x2 = !(i > j) && k > l;
-	printf("%d,%d\n", x1, x2);
+	int sum = 0, i = 1;
+	while (i <= 10)
+	{
+		sum = sum + i;
+		i++;
+	}
+	cout << sum << endl;
 	system("pause");
 }
 
 
-//强制类型的转换（类型说明符（表达式）或者（类型说明符）表达式）
+//do...while循环（do...while语句后面必须要有分号）：实现累加
 #include<iostream>
 #include<windows.h>
 using namespace std;
 void main()
 {
-	float i, j;
-	int k;
-	i = 60.25;
-	j = 20.5;
-	k = (int)i + (int)j;
-	cout << k << endl;
+	int sum = 0, i = 1;
+	do
+	{
+		sum = sum + i;
+		i++;
+	} while (i <= 10);
+	cout << sum << endl;
 	system("pause");
 }
 
-
-
-//判断语句：根据成绩划分等级
+//for循环：累加(若要省略for中的3个表达式，则该循环将会陷入死循环，省略了表达式，但是两个分号是不能被省略)
 #include<iostream>
 #include<windows.h>
 using namespace std;
 void main()
 {
-	int grade;
-	cin >> grade;
-	if (grade >= 90)
+	int sum = 0;
+	int i;
+	for (i = 1; i <= 10; i++)
 	{
-		cout << "very good" << endl;
+		sum = sum + i;
 	}
-	else if (grade >= 80 && grade < 90)
-	{
-		cout << "good" << endl;
-	}
-	else if (grade >= 70 && grade < 80)
-	{
-		cout << "good" << endl;
-	}
-	else if (grade >= 60 && grade < 70)
-
-	{
-		cout << "normal" << endl;
-	}
-	else if (grade < 60)
-	{
-		cout << "failure" << endl;
-	}
+	cout << sum << endl;
 	system("pause");
 }
 
-
-//条件表达式：判断一个数是否为3和5的整倍数(关系表达式？表达式1：表达式2)
+//break语句：用break语句跳出当前循环
 #include<iostream>
 #include<windows.h>
 using namespace std;
 void main()
+{
+	int i, n, sum;
+	sum = 0;
+	cout << "input 10 number" << endl;
+	for (i = 1; i <= 10; i++)
+	{
+		cout << i << ":" << ;
+		cin >> n;
+		if (n < 0)
+			break;
+		sum = sum + n;
+	}
+	cout << sum << endl;
+	system("pause");
+}
+
+
+//continue语句：跳出本次循环
+#include<iostream>
+#include<windows.h>
+using namespace std;
+void main()
+{
+	int i, n, sum;
+	sum = 0;
+	cout << "input 10 number" << endl;
+	for (i = 1; i <= 10; i++)
+	{
+		cout << i << ":" << ;
+		cin >> n;
+		if (n < 0)
+			continue;
+		sum = sum + n;
+	}
+	cout << sum << endl;
+	system("pause");
+}
+
+
+//打印三角
+#include<iostream>
+#include<windows.h>
+using namespace std;
+void main()
+{
+	int i, j, k;
+	for (i = 1; i <= 5; i++)//控制行数
+	{
+		for (j = 1; j <= 5 - i; j++)//控制空格数
+			cout << " ";
+		for (k = 1; k <= 2 * i - 1; k++)//控制打印*的数量
+			cout << "*";
+		cout << endl;
+	}
+	system("pause");
+}
+
+//输出乘法口诀表
+#include<iostream>
+#include<iomanip>
+#include<windows.h>
+using namespace std;
+void main()
+{
+	int i, j;
+	i = 1;
+	j = 1;
+	for (i = 1; i < 10; i++)
+	{
+		for (j = 1; j < i + 1; j++)
+			cout << setw(2) << i << "*" << j << "=" << setw(2) << i*j;//setw(int w)设置下一个数据的输出域宽为w
+		cout << endl;
+	}
+	system("pause");
+}
+
+//巴斯卡三角形：两个边全输出1，三角形内部用上行相邻两个数之和，各相加后的和数等分的放在三角形的各行内
+#include<iostream>
+#include<iomanip>
+#include<windows.h>
+using namespace std;
+long combi(int n, int r)
 {
 	int i;
-	cout << "输入i的值" << endl;
-	cin >> i;
-	(i % 3 == 0 && i % 5 == 0) ? cout << "yes" : cout << "no";
-	cout << endl;
+	long p = 1;
+	for (i = 1; i <= r; i++)
+		p = p*(n - i + 1) / i;
+	return p;
+}
+void main()
+{
+	int n, r, t;
+	for (n = 0; n <= 12; n++)//控制行数
+	{
+		for (r = 0; r <= n; r++)
+		{
+			int i;
+			if (r == 0)
+			{
+				for (i = 0; i <= (12 - n); i++)
+					cout << " ";//每行每一个元素的位置
+			}
+			else
+				cout << " ";//每个数之间空两格
+			cout << setw(3) << combi(n, r);
+		}
+		cout << endl;
+	}
 	system("pause");
 }
 
-
-//switch语句：根据输入的字符输出字符串（若匹配成功用break跳出循环，表达式是算数表达式，因此该值的类型为整形或字符型）
+//阿姆斯壮数：在三位整数中形如：153=1^3+5^3+3^3
 #include<iostream>
 #include<windows.h>
 using namespace std;
-void main()
+int main()
 {
-	char i;
-	cin >> i;
-	switch (i)
+	int a, b, c;
+	int i;
+	for (i = 100; i <= 999; i++)
 	{
-	case'A':
-			cout << "very good" << endl;
-			break;
-	case'B':
-				cout << "good" << endl;
-				break;
-	case'C':
-					cout << "normal" << endl;
-	case'D':
-						cout << "failure" << endl;
-					defalut:
-						cout << "i error" << endl;
+		a = i / 100;//求百位
+		b = (i % 100) / 10;//求十位
+		c = i % 10;//求个位
+		if (a*a*a + b*b*b + c*c*c == i)
+			cout << i << endl;
 	}
-	system("pasue");
-}
-
-//判断语句的嵌套:判断是否为闰年
-#include<iostream>
-#include<windows.h>
-using namespace std;
-void main()
-{
-	int Year;
-	cin >> Year;
-	if (Year % 4 == 0)
-	{
-		if (Year % 100 == 0)
-		{
-			if (Year % 400 == 0)
-				cout << "是闰年" << endl;
-			else
-				cout << "不是闰年" << endl;
-		}
-		else
-			cout << "是闰年" << endl;
-	}
-	else
-		cout << "不是闰年" << endl;
-	system("pasue");
+	system("pause");
+	return 0;
 }
 
